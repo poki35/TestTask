d70cfec6d3886d519048d861da7871581c786f4a
poki35 <kirillponomarenko322@gmail.com>
2022-11-21 00:11:13 +0300

    Initial Commit

---
+++ Tree/TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeApp.swift
+//  Tree
+//
+//  Created by Кирилл Пономаренко on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
